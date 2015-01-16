9148436824f09a06544b9a3cdb4d300074c195b8
jimwhimpey <jimwhimpey@me.com>
2015-01-15 20:41:38 -0800

    Started interaction stuff

--- build/unitSwitcher.js
+++ build/unitSwitcher.js
@@ -1,10 +1,21 @@
 var UnitSwitcher = React.createClass({displayName: "UnitSwitcher",
+	
+	handleClick: function(e) {
+		
+		e.preventDefault();
+		
+		console.log("e", e);
+		
+		return;
+	},
+	
 	render: function() {
 		return (
 			React.createElement("ul", {class: "unit-switcher"}, 
-				React.createElement("li", null, React.createElement("a", {href: "/"}, "metric")), 
-				React.createElement("li", null, React.createElement("a", {href: "/"}, "imperial"))
+				React.createElement("li", null, React.createElement("a", {href: "#", onClick: this.handleClick}, "metric")), 
+				React.createElement("li", null, React.createElement("a", {href: "#", onClick: this.handleClick}, "imperial"))
 			)
 		);
 	}
+	
 });
--- components/unitSwitcher.js
+++ components/unitSwitcher.js
@@ -1,10 +1,21 @@
 var UnitSwitcher = React.createClass({
+	
+	handleClick: function(e) {
+		
+		e.preventDefault();
+		
+		console.log("e", e);
+		
+		return;
+	},
+	
 	render: function() {
 		return (
 			<ul class="unit-switcher">
-				<li><a href="/">metric</a></li>
-				<li><a href="/">imperial</a></li>
+				<li><a href="#" onClick={this.handleClick}>metric</a></li>
+				<li><a href="#" onClick={this.handleClick}>imperial</a></li>
 			</ul>
 		);
 	}
+	
 });
