837b44f2d0f41bc9c8618e68faca3f0bf0a58a24
jimwhimpey <jimwhimpey@me.com>
2015-01-28 11:56:01 -0800

    Finished styling the data section

--- build/mileageData.js
+++ build/mileageData.js
@@ -15,21 +15,21 @@ var MileageData = React.createClass({
 			React.createElement("div", {className: "mileageData"}, 
 			
 				React.createElement("ul", {className: "yearToDate"}, 
-					React.createElement("li", null, "This year's distance: ", formatNumber(this.props.rideData.ytdDistance, '0,0.00', 'big', this.state.isMetric)), 
-					React.createElement("li", null, "This year's elevation gain: ", formatNumber(this.props.rideData.ytdElevation, '0,0.00', 'small', this.state.isMetric))
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdDistance, '0,0', 'big', this.state.isMetric)), " This year's distance"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdElevation, '0,0', 'small', this.state.isMetric)), " This year's elevation gain")
 				), 
 			
 				React.createElement("ul", {className: "averageYearToDate"}, 
-					React.createElement("li", null, "Average distance per day this year: ", formatNumber(this.props.rideData.ytdDistancePerDay, '0,0', 'big', this.state.isMetric)), 
-					React.createElement("li", null, "Average elevation gain per day this year: ", formatNumber(this.props.rideData.ytdElevationPerDay, '0,0', 'small', this.state.isMetric)), 
-					React.createElement("li", null, "Average distance per week this year: ", formatNumber(this.props.rideData.ytdDistancePerWeek, '0,0', 'big', this.state.isMetric)), 
-					React.createElement("li", null, "Average elevation gain per week this year: ", formatNumber(this.props.rideData.ytdElevationPerWeek, '0,0', 'small', this.state.isMetric))
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdDistancePerDay, '0,0', 'big', this.state.isMetric)), " Average distance per day this year"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdElevationPerDay, '0,0', 'small', this.state.isMetric)), " Average elevation gain per day this year"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdDistancePerWeek, '0,0', 'big', this.state.isMetric)), " Average distance per week this year"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.ytdElevationPerWeek, '0,0', 'small', this.state.isMetric)), " Average elevation gain per week this year")
 				), 
 				
 				React.createElement("ul", {className: "currentProjection"}, 
-					React.createElement("li", null, "Days left in year: ", this.props.rideData.daysLeftInYear), 
-					React.createElement("li", null, "Projected annual distance: ", formatNumber(this.props.rideData.projectedAnnualDistance, '0,0', 'big', this.state.isMetric)), 
-					React.createElement("li", null, "Projected annual elevation: ", formatNumber(this.props.rideData.projectedAnnualElevation, '0,0', 'small', this.state.isMetric))
+					React.createElement("li", null, React.createElement("span", {className: "data"}, this.props.rideData.daysLeftInYear), " Days left in year"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.projectedAnnualDistance, '0,0', 'big', this.state.isMetric)), " Projected annual distance"), 
+					React.createElement("li", null, React.createElement("span", {className: "data"}, formatNumber(this.props.rideData.projectedAnnualElevation, '0,0', 'small', this.state.isMetric)), " Projected annual elevation gain")
 				)
 			
 			)
--- components/mileageData.js
+++ components/mileageData.js
@@ -15,21 +15,21 @@ var MileageData = React.createClass({
 			<div className="mileageData">
 			
 				<ul className="yearToDate">
-					<li>This year's distance: {formatNumber(this.props.rideData.ytdDistance, '0,0.00', 'big', this.state.isMetric)}</li>
-					<li>This year's elevation gain: {formatNumber(this.props.rideData.ytdElevation, '0,0.00', 'small', this.state.isMetric)}</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdDistance, '0,0', 'big', this.state.isMetric)}</span> This year's distance</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdElevation, '0,0', 'small', this.state.isMetric)}</span> This year's elevation gain</li>
 				</ul>
 			
 				<ul className="averageYearToDate">
-					<li>Average distance per day this year: {formatNumber(this.props.rideData.ytdDistancePerDay, '0,0', 'big', this.state.isMetric)}</li>
-					<li>Average elevation gain per day this year: {formatNumber(this.props.rideData.ytdElevationPerDay, '0,0', 'small', this.state.isMetric)}</li>
-					<li>Average distance per week this year: {formatNumber(this.props.rideData.ytdDistancePerWeek, '0,0', 'big', this.state.isMetric)}</li>
-					<li>Average elevation gain per week this year: {formatNumber(this.props.rideData.ytdElevationPerWeek, '0,0', 'small', this.state.isMetric)}</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdDistancePerDay, '0,0', 'big', this.state.isMetric)}</span> Average distance per day this year</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdElevationPerDay, '0,0', 'small', this.state.isMetric)}</span> Average elevation gain per day this year</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdDistancePerWeek, '0,0', 'big', this.state.isMetric)}</span> Average distance per week this year</li>
+					<li><span className="data">{formatNumber(this.props.rideData.ytdElevationPerWeek, '0,0', 'small', this.state.isMetric)}</span> Average elevation gain per week this year</li>
 				</ul>
 				
 				<ul className="currentProjection">
-					<li>Days left in year: {this.props.rideData.daysLeftInYear}</li>
-					<li>Projected annual distance: {formatNumber(this.props.rideData.projectedAnnualDistance, '0,0', 'big', this.state.isMetric)}</li>
-					<li>Projected annual elevation: {formatNumber(this.props.rideData.projectedAnnualElevation, '0,0', 'small', this.state.isMetric)}</li>
+					<li><span className="data">{this.props.rideData.daysLeftInYear}</span> Days left in year</li>
+					<li><span className="data">{formatNumber(this.props.rideData.projectedAnnualDistance, '0,0', 'big', this.state.isMetric)}</span> Projected annual distance</li>
+					<li><span className="data">{formatNumber(this.props.rideData.projectedAnnualElevation, '0,0', 'small', this.state.isMetric)}</span> Projected annual elevation gain</li>
 				</ul>
 			
 			</div>
